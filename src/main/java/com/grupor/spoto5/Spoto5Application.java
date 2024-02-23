eae7b5e3705dab91c98c4cb682de7cdfb690cb98
apachete <ddaassmg@gmail.com>
2024-02-23 12:34:37 +0100

    Inicial commit

---
+++ src/main/java/com/grupor/spoto5/Spoto5Application.java
@@ -0,0 +1,13 @@
+package com.grupor.spoto5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spoto5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spoto5Application.class, args);
+	}
+
+}
