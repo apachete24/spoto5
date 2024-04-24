7491f42b19ae8cca6e0c5802aaff1aa0ac999e86
mariomorenonieto <m.morenon.2020@alumnos.urjc.es>
2024-04-24 18:18:11 +0200

    Tema usuarios primeros avances, lineas comentadas para poder seguir modificando

---
+++ src/main/java/com/grupor/spoto5/controller/WebController.java
@@ -0,0 +1,27 @@
+/*package com.grupor.spoto5.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    /*@GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/loginerror")
+    public String loginerror() {
+        return "loginerror";
+    }
+    @GetMapping("/private")
+    public String privatePage() {
+        return "private";
+    }
+}
+
+ */
